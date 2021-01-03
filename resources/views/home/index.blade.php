c34606c4f9155a6b7cc05fa3fb269bea07be142e
inggolanda <inggolandaa@gmail.com>
2021-01-03 21:03:39 +0700

    Project Ecommerce

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RedStore | Ecommerce </title>
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/font.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="assets/img/logo.png" width="125">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Products</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Account</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <h1>Give Your Workout <br> A New Style</h1>
+                    <p>Succeess isn't always about greatness. It's about consistency.
+                        Consistent <br> hard work gains success.
+                        Greatness will come.</p>
+                    <a href="" class="btn">Explore now &#8594;</a>
+                </div>
+                <div class="col-2">
+                    <img src="assets/img/image1.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
