8adf356406935e5825b962f1d8aa796054b5d781
inggolanda <inggolandaa@gmail.com>
2021-01-05 22:26:11 +0700

    Last Product dan exclusive product

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
     <title>RedStore | Ecommerce </title>
 
     <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/font.css">
+    <!--<link rel="stylesheet" href="assets/css/fontawesome.min.css">-->
 </head>
 
 <body>
@@ -67,31 +67,126 @@
             <div class="col-4">
                 <img src="assets/img/product-1.jpg">
                 <h4>Red Printed T-Shirt</h4>
-                <p>****</p>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
                 <p>$50.00</p>
             </div>
             <div class="col-4">
                 <img src="assets/img/product-2.jpg">
                 <h4>Red Printed T-Shirt</h4>
-                <p>****</p>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
                 <p>$50.00</p>
             </div>
             <div class="col-4">
                 <img src="assets/img/product-3.jpg">
                 <h4>Red Printed T-Shirt</h4>
-                <p>****</p>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
                 <p>$50.00</p>
             </div>
             <div class="col-4">
                 <img src="assets/img/product-4.jpg">
                 <h4>Red Printed T-Shirt</h4>
-                <p>****</p>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+        </div>
+        <h2 class="title">Latest Products</h2>
+        <div class="row">
+            <div class="col-4">
+                <img src="assets/img/product-5.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-6.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-7.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-8.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="assets/img/product-9.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-10.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-11.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
+                <p>$50.00</p>
+            </div>
+            <div class="col-4">
+                <img src="assets/img/product-12.jpg">
+                <h4>Red Printed T-Shirt</h4>
+                <div class="rating">
+                    <i class="fa fa-start"></i>
+                </div>
                 <p>$50.00</p>
             </div>
 
         </div>
     </div>
 
+    <!------------ Featured Products------->
+    <div class="offer">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-2">
+                    <img src="assets/img/exclusive.png" class="offer-img">
+                </div>
+                <div class="col-2">
+                    <p>Exclusively Available on RedStore</p>
+                    <h1>Smart Band 4</h1>
+                    <small> The Mi Smart Band 4 features a 39.9% larger
+                        (than Mi Band 3) AMOLED color full-touch display with
+                        adjustable brightness, so everyting is clear as can
+                        be.</small>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
