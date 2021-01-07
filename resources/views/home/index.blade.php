3f9352b5cd0a127238f3bb94d8bcafc20a9fbbd5
inggolanda <inggolandaa@gmail.com>
2021-01-07 20:21:02 +0700

    menambahkan Testimonial

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,6 +7,9 @@
     <title>RedStore | Ecommerce </title>
 
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!--<link rel="stylesheet" href="assets/css/fontawesome.min.css">-->
 </head>
 
@@ -68,7 +71,11 @@
                 <img src="assets/img/product-1.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -76,7 +83,11 @@
                 <img src="assets/img/product-2.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-0"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -84,7 +95,11 @@
                 <img src="assets/img/product-3.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-0"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -92,7 +107,11 @@
                 <img src="assets/img/product-4.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -103,7 +122,11 @@
                 <img src="assets/img/product-5.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -111,7 +134,11 @@
                 <img src="assets/img/product-6.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -119,7 +146,11 @@
                 <img src="assets/img/product-7.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -127,7 +158,11 @@
                 <img src="assets/img/product-8.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -137,7 +172,11 @@
                 <img src="assets/img/product-9.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -145,7 +184,11 @@
                 <img src="assets/img/product-10.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -153,7 +196,11 @@
                 <img src="assets/img/product-11.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -161,7 +208,11 @@
                 <img src="assets/img/product-12.jpg">
                 <h4>Red Printed T-Shirt</h4>
                 <div class="rating">
-                    <i class="fa fa-start"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half"></i>
                 </div>
                 <p>$50.00</p>
             </div>
@@ -183,10 +234,60 @@
                         (than Mi Band 3) AMOLED color full-touch display with
                         adjustable brightness, so everyting is clear as can
                         be.</small>
+                    <a href="" class="btn">Buy Now &#8594;</a>
                 </div>
             </div>
         </div>
     </div>
+    <!------Testimoni------>
+    <div class="testimonial">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Tmaksdmsdnwjasdnsmmdkwmsmdnamasm</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <img src="assets/img/user-1.png">
+                    <h3>Sean Parker</h3>
+                </div>
+
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Tmaksdmsdnwjasdnsmmdkwmsmdnamasm</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <img src="assets/img/user-2.png">
+                    <h3>Sean Parker</h3>
+                </div>
+
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Tmaksdmsdnwjasdnsmmdkwmsmdnamasm</p>
+                    <div class="rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-half"></i>
+                    </div>
+                    <img src="assets/img/user-3.png">
+                    <h3>Sean Parker</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
